52dbc53717e591906e0bcc98a93c78603e7096f5
OhByeongYun <01dreamer@naver.com>
2013-12-31 23:12:25 +0900

    init

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
+// general module import
 var http = require('http')
-  , httpProxy = require('http-proxy');
+  , express = require('express');
+  
+
+// create server
+var app = express();
+var server = http.createServer(app).listen(5000);
+
+
+// express global setting
+app.use(express.static(__dirname + '/public/'));
+
+
+// route
+app.get('/', function(req, res){
+	res.sendfile('public/fake_index.html');
+});
+
+app.get('/develop', function(req, res) {
+	res.sendfile('public/real_index.html');
+});
