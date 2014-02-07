e708d1fbff0bcee6ade9e9eb426c6039a163bf51
OhByeongYun <01dreamer@naver.com>
2014-02-07 23:17:01 +0900

    !warn - stop this repo

--- public/js/media.js
+++ public/js/media.js
@@ -1,3 +1,67 @@
+/*
+ * musicplayer
+ *
+ *
+ * Copyright (c) 2014 OhByeongYun
+ * Licensed under the MIT license.
+ *
+ * Date: 2014-01-08
+ */
+ 
+(function(global){
+	"use strict";
+	
+	var $ = document.querySelector.bind(document);
+	var $$ = document.createElement.bind(document);
+	
+	var MusicPlayer = (function(){
+		
+		var MusicPlayer = function(selector) {
+			return new MusicPlayer.fn.ctor(selector);
+		};
+		
+		MusicPlayer.fn = MusicPlayer.prototype = {
+			ctor: function(selector) {
+				var domWrap = $(selector);
+				
+				var domAudioPlayer = $$("div");
+				domAudioPlayer.id = "audioplayer";
+				
+				var domController = $$("div");
+				domController.className = "controller";
+				domAudioPlayer.appendChild(domController);
+				
+				var domTimeline = $$("div");
+				domTimeline.className = "timeline";
+				domAudioPlayer.appendChild(domTimeline);
+				
+				domWrap.appendChild(domAudioPlayer);
+			}
+		};
+		
+		MusicPlayer.fn.ctor.prototype = MusicPlayer.fn;
+		
+		// EXTEND
+		MusicPlayer.init = MusicPlayer.fn.init = function(options) {
+			console.log(options);
+			
+			return 1;
+		};
+		
+		// EXPOSE
+		return (global.MusicPlayer = global.__ = MusicPlayer);
+	})();
+})(window);
+
+/*
+MusicPlayer.extend = MusicPlayer.fn.extend = function() {
+	console.log('extend');
+	
+	return this;
+};
+*/
+
+/*
 var MediaPlayer = (function(){
 	var _state = "stop";
 	var _isVolue = false;
@@ -14,6 +78,9 @@ var MediaPlayer = (function(){
 	};
 	
 	var play = function(music) {
+		var title = music.title;
+		var artist = music.artist;
+		
 	};
 	
 	var _createAudioPlayer = function() {
@@ -22,12 +89,12 @@ var MediaPlayer = (function(){
 	
 	var _resize = function() {
 		var func = function() {
-			var _wWindow = window.innerWidth;
-			if(_wWindow < 1000) {
-				_wWindow = 1000;
+			var _width = window.innerWidth;
+			if(_width < 1000) {
+				_width = 1000;
 			}
-			var _wControls = $("#audioplayer .controls").width();
-			$("#audioplayer .nowplaying").width(_wWindow - _wControls);
+			var _controls_width = $("#audioplayer .controls").width();
+			$("#audioplayer .nowplaying").width(_width - _controls_width);
 		};
 		func();
 		$(window).resize(func);
@@ -38,3 +105,4 @@ var MediaPlayer = (function(){
 		play: play
 	};
 })();
+*/
